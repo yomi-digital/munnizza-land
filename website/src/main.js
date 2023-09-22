39be07d3483b7d57784a73d808460a0dc7c6a4e5
turinglabs <sebastiano.cataudo@gmail.com>
2023-09-22 08:40:48 +0200

    removing mapbox references

--- website/src/main.js
+++ website/src/main.js
@@ -3,6 +3,5 @@ import './assets/themes/style.scss'
 import './assets/themes/resp.scss'
 import './assets/fonts/stylesheet.css'
 import App from './App.vue'
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 createApp(App).mount('#app')
